cf8ef706fd78eb48e78bd3a5650a66759ea99ec2
JacksonSM <jacksons.m2018@gmail.com>
2022-08-24 17:36:08 -0300

    Definido os models de corrida.

---
+++ Corrida.API/Model/CorridaContext/Corrida.cs
@@ -0,0 +1,14 @@
+﻿namespace Corrida.API.Model.CorridaContext;
+
+public class Corrida
+{
+    public string CorridaId { get; set; } = string.Empty;
+    public int PassageiroId { get; set; }
+    public int? MotoTaxistaId { get; set; }
+    public Localizacao Origem { get; set; } = new Localizacao();
+    public Localizacao Destino { get; set; } = new Localizacao();
+    public string Estado { get; set; } = string.Empty;
+    public float Estrelas { get; set; }
+
+
+}
---
+++ Corrida.API/Model/CorridaContext/Localizacao.cs
@@ -0,0 +1,10 @@
+﻿namespace Corrida.API.Model.CorridaContext
+{
+    public class Localizacao
+    {
+        public string Cidade { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public string Rua { get; set; } = string.Empty;
+        public string PontoReferencia { get; set; } = string.Empty;
+    }
+}
